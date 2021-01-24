2910c7a8f16ed146daf1bad022e5c4253aa29e32
ericsmith92 <ericsmth884@gmail.com>
2021-01-24 13:33:02 -0500

    playing with routing and navagation in our single page app (SPA)

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => {
+//history available to this component because we wrapped it with withRouter
+//we didn't access this like other props through drilling
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
     return(
-        <div className={`menu-item ${size ? size : ''}`}>
+        <div className={`menu-item ${size ? size : ''}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
             <div className='background-image' style={{background: `url(${imageUrl})`}} />
             <div className='content'>
                 <h2 className='title'>
@@ -18,4 +21,7 @@ const MenuItem = ({ title, imageUrl, size }) => {
     )
 }
 
-export default MenuItem;
+//withRouter is a higher order component, this means we basically use it to 
+//wrap other components, and it returns us back a 'powered up' version of the component
+//in this case, we are wrapping MenuItem to give it some extra juice (access to history object)
+export default withRouter(MenuItem);
